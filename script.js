70f006831556976f495ab3390364a78e6919f302
Viktor <vikvzm@gmail.com>
2024-08-04 23:43:07 +0300

    added iterftion

--- script.js
+++ script.js
@@ -9,8 +9,29 @@ const personalMovieDB = {
     genres: [],
     prifat: false
 };
-const movie = prompt('Один из последних просмотренных фильмов?', '');
-const rating = prompt('На сколько оцените его?', '');
-personalMovieDB.movies[movie] = rating;
+
+for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 1; j++) {
+        const movie = prompt('Один из последних просмотренных фильмов?', '');
+        const rating = prompt('На сколько оцените его?', '');
+        if (movie && rating && movie.length < 50) {
+            personalMovieDB.movies[movie] = rating;
+        } else {
+            j--;
+            console.log('Вы ввели пустую строку, попробуйте еще раз')
+        }
+    }
+}
+
+if (personalMovieDB["count"] > 0 && personalMovieDB["count"] < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB["count"] >= 10 && personalMovieDB["count"] < 30) {
+    console.log('Вы класический зритель');
+} else if (personalMovieDB["count"] >= 30) {
+    console.log('Вы киноман')
+} else {
+    console.log('Произошла ошибка')
+}
+
 
 console.log(personalMovieDB)
